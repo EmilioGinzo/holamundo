4df1b2c04fc4a570437421f8305d931b06a76858
nicolasespinola <77073121+nicolasespinola@users.noreply.github.com>
2021-07-25 21:20:52 -0400

    Commit de Nico

--- holamundo.py
+++ holamundo.py
@@ -2,4 +2,7 @@
 print("hola mundo")
 print("hola mundo escrito por ivan weiss jajaja")
 print("otro hola mundo por las dudas")
+
+if (0==0):
+  print("Holaa soy Nico y soy mejor que ustedes jaja :) ")
 #comentario :)
